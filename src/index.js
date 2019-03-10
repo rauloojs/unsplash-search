117ff7cb9ea7358620e3c292a6812cf73b652c0c
Raúl Ojeda <rauloojs@gmail.com>
2019-03-10 12:39:00 -0600

    chore(index): Import bootstrap css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './store';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
