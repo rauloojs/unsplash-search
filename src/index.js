15001fa9a2255785e1c3bf947c2d7f5987e2554f
Raúl Ojeda <rauloojs@gmail.com>
2019-03-10 12:47:00 -0600

    chore(index): Update reference to Appcontainer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import AppContainer from './containers/AppContainer';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +9,6 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>
   , document.getElementById('root'));
