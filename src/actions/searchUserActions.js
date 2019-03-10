1d84bb6d896776c193b840dddd4fd6be5398652a
Raúl Ojeda <rauloojs@gmail.com>
2019-03-10 13:42:19 -0600

    feature(actions searchUserActions): Add actions for searching users

---
+++ src/actions/searchUserActions.js
@@ -0,0 +1,28 @@
+import * as SEARCH_USER_CONSTANTS from '../constants/searchUserConstants';
+import unsplashService from '../services/unsplashService';
+
+export const searchUser = (query) => {
+  return (dispatch) => {
+    dispatch(request(query));
+
+    unsplashService.searchUser(query)
+      .then((response) => {
+        dispatch(success(response));
+      });
+
+    function request (query) {
+      return {
+        type: SEARCH_USER_CONSTANTS.SEARCH_REQUEST,
+        query,
+      }
+    }
+
+    function success (response) {
+      return {
+        type: SEARCH_USER_CONSTANTS.SEARCH_SUCCESS,
+        ...response,
+      }
+    }
+  };
+};
+
